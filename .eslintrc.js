2432679e9fd88dba3cb8931d2caa4f15b8ad4c20
kbmelo <kbmelo@outlook.com>
2021-11-16 09:44:19 -0300

    removing craco and extending airbnb

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'import',
+  ],
+  ignorePatterns: [
+    '*/.js',
+    'node_modules/*',
+    'build/*',
+    'dist/*',
+    '*.css',
+    '*.svg',
+    '*.json',
+    '*.ejs',
+    '*.js',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/no-unresolved': [
+      2, 
+      { caseSensitive: false },
+    ],
+  },
+};
